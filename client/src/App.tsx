8d9abc1fae79532c8cdec7dba859d844add35a37
JMakoti <josephmwamuye11@gmail.com>
2025-11-16 11:08:20 +0300

    chore: reinitialize the project structure from js to ts

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Card } from "./components/ui/card";
+
+function App() {
+  return (
+    <>
+      <Card className="w-96 mx-auto mt-20 p-6">
+        <h1 className="text-2xl font-bold mb-4 text-center">Welcome to Kifaru!</h1>
+      </Card>
+    </>
+  );
+}
+
+export default App;
